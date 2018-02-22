b1ff05068894447ceab2ecd5722394da5cd6e3d6
Loosper <boian4o1@gmail.com>
2018-02-22 10:22:16 +0200

    Different users can connect on different links

--- server/server.py
+++ server/server.py
@@ -4,7 +4,7 @@ import tornado_xstatic
 import os
 import webbrowser
 
-from terminado import TermSocket, SingleTermManager
+from terminado import TermSocket, NamedTermManager
 from terminado import uimodule
 
 
@@ -12,30 +12,20 @@ STATIC_DIR = os.path.join(os.path.dirname(__file__), "../client")
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), "../client")
 
 
-def run_and_show_browser(url, term_manager):
-    loop = tornado.ioloop.IOLoop.instance()
-    loop.add_callback(webbrowser.open, url)
-    try:
-        loop.start()
-    except KeyboardInterrupt:
-        print(" Shutting down...")
-    finally:
-        term_manager.shutdown()
-        loop.close()
-
-
 class TerminalPageHandler(tornado.web.RequestHandler):
-    def get(self):
-        return self.render("index.html", static=self.static_url,
-                           xstatic=self.application.settings['xstatic_url'],
-                           ws_url_path="/websocket")
+    def get(self, path):
+        return self.render(
+            "index.html", static=self.static_url,
+            xstatic=self.application.settings['xstatic_url'],
+            ws_url_path="/websocket/" + path
+        )
 
 
 def main(argv):
-    term_manager = SingleTermManager(shell_command=['bash'])
+    term_manager = NamedTermManager(3, shell_command=['zsh'])
     handlers = [
-        (r"/websocket", TermSocket, {'term_manager': term_manager}),
-        (r"/", TerminalPageHandler),
+        (r"/websocket/(.*)", TermSocket, {'term_manager': term_manager}),
+        (r"/(.*)", TerminalPageHandler),
         (r"/xstatic/(.*)", tornado_xstatic.XStaticFileHandler,
             {'allowed_modules': ['termjs']})
     ]
@@ -46,8 +36,20 @@ def main(argv):
         xstatic_url=tornado_xstatic.url_maker('/xstatic/')
     )
 
+    PORT = 8765
+    print('Listening on port {}.'.format(PORT))
+
     app.listen(8765, 'localhost')
-    run_and_show_browser("http://localhost:8765/", term_manager)
+    loop = tornado.ioloop.IOLoop.instance()
+    loop.add_callback(webbrowser.open, "http://localhost:{}/".format(PORT))
+
+    try:
+        loop.start()
+    except KeyboardInterrupt:
+        print(" Shutting down...")
+    finally:
+        term_manager.shutdown()
+        loop.close()
 
 
 if __name__ == '__main__':
