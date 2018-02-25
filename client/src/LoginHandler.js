da27f6bac2a3b94c72ada70461383241e7fbd815
Loosper <boian4o1@gmail.com>
2018-02-25 18:20:29 +0200

    Removed useless stuff

--- client/src/LoginHandler.js
+++ client/src/LoginHandler.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
+
 import './styles/login.css';
-import './styles/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 class LoginHandler extends Component {
-    
-      
     render() {
         return (
-            <div class="container">  
-                <div class="col-lg-6 col-md-6 col-sm-8  loginbox">
-                    <div class=" row">
-                        <div class="col-md-12">
-                            <div class="singtext">Sherminal</div>   
+            <div className="container">
+                <div className="col-lg-6 col-md-6 col-sm-8  loginbox">
+                    <div className=" row">
+                        <div className="col-md-12">
+                            <div className="singtext">Sherminal</div>
                         </div>
-                                 
+
                     </div>
-                    <div class=" row loginbox_content ">                        
-                        <div class="col-md-12" >
-                            <input class="form-control" type="text" placeholder="User name"  />
+                    <div className=" row loginbox_content ">
+                        <div className="col-md-12" >
+                            <input className="form-control" type="text" placeholder="User name"  />
                         </div>
                     </div>
-                    <div class="row ">                   
-                        <div class="col-md-12 login-btn-container">                        
-                            <a href="#" class=" btn btn-secondary">Sign in <span class="glyphicon glyphicon-log-in"></span> </a> 
+                    <div className="row ">
+                        <div className="col-md-12 login-btn-container">
+                            <a className=" btn btn-secondary">Sign in <span className="glyphicon glyphicon-log-in"></span> </a>
                         </div>
                     </div>
                 </div>
@@ -32,6 +31,4 @@ class LoginHandler extends Component {
 }
 
 
-
 export default LoginHandler;
-
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import './styles/index.css';
 import LoginHandler from './LoginHandler';
 
-//ReactDOM.render(<App />, document.getElementById('root'));
 ReactDOM.render(<LoginHandler />, document.getElementById('login'));
-
-registerServiceWorker();
