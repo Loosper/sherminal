794c04c367732700288e4202b2437ba3a04c22b0
Loosper <boian4o1@gmail.com>
2018-02-25 14:14:19 +0200

    A login path

--- server/server.py
+++ server/server.py
@@ -1,29 +1,34 @@
-import tornado.web
+import json
 import os
-import webbrowser
+# import webbrowser
 # import asyncio
+import tornado.web
 import tornado.options
 
-from tornado.web import StaticFileHandler
+from tornado.web import StaticFileHandler, RequestHandler
 from tornado.platform.asyncio import AsyncIOMainLoop
 
-# from sqlalchemy import create_engine
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 from terminado import TermSocket, NamedTermManager
 
 
+from database import Base, User
+
 DIR = os.path.dirname(__file__)
 
 
-class TerminalPageHandler(tornado.web.RequestHandler):
-    async def get(self, path):
-        return self.render(
-            "index.html", static=self.static_url,
-            ws_url_path="/websocket/" + path
-        )
+# class TerminalPageHandler(tornado.web.RequestHandler):
+#     async def get(self, path):
+#         return self.render(
+#             "index.html", static=self.static_url,
+#             ws_url_path="/websocket/" + path
+#         )
 
 
-class Static(tornado.web.StaticFileHandler):
+# REVIEW: dir() and help()
+class Static(StaticFileHandler):
     def initialize(self, path):
         self.dirname, self.filename = os.path.split(path)
         super().initialize(self.dirname)
@@ -32,18 +37,47 @@ class Static(tornado.web.StaticFileHandler):
         super().get(self.filename, include_body)
 
 
+class Login(RequestHandler):
+    def initialize(self, database):
+        self.session = database()
+
+    # TODO: await databse connections
+    async def post(self):
+        if self.request.headers["Content-Type"].startswith("application/json"):
+            try:
+                data = json.loads(self.request.body)
+            except json.JSONDecodeError:
+                self.send_error(400)
+                return
+        else:
+            return
+
+        user = self.session.query(User).filter_by(
+            username=data['username']).first()
+
+        if not user:
+            new_user = User(username=data['username'])
+            self.session.add(new_user)
+            self.session.commit()
+
+        self.write({'terminal_path': data['username']})
+
+
 def main(argv):
     AsyncIOMainLoop().install()
     # logging
     tornado.options.parse_command_line()
 
-    # engine = create_engine('sqlite:///:memory:', echo=True)
+    engine = create_engine('sqlite:///:memory:', echo=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
 
     term_manager = NamedTermManager(3, shell_command=['zsh'])
     handlers = [
         (r"/websocket/(.*)", TermSocket, {'term_manager': term_manager}),
         (r"/", Static,
             {'path': os.path.join(DIR, "../client/index.html")}),
+        (r'/login', Login, {'database': Session}),
         (r'/static/(setup\.js)', StaticFileHandler,
             {'path': os.path.join(DIR, "../client/")}),
         (r'/static/(.*)', StaticFileHandler,
@@ -51,7 +85,8 @@ def main(argv):
     ]
 
     app = tornado.web.Application(
-        handlers
+        handlers,
+        debug=True
     )
 
     PORT = 8765
@@ -60,7 +95,7 @@ def main(argv):
     app.listen(PORT, 'localhost')
 
     loop = tornado.ioloop.IOLoop.instance()
-    loop.add_callback(webbrowser.open, "http://localhost:{}/".format(PORT))
+    # loop.add_callback(webbrowser.open, "http://localhost:{}/".format(PORT))
 
     try:
         loop.start()
