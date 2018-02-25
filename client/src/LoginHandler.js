39a70e4c4e1a90649e06239c0a3005b446b10a42
denishristov <plaritus@gmail.com>
2018-02-25 17:48:20 +0200

    started react app

---
+++ client/src/LoginHandler.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './styles/login.css';
+import './styles/bootstrap.min.css';
+
+class LoginHandler extends Component {
+    var Component = React.createClass({
+        getInitialState: function() {
+          return {
+            inputValue: ''
+          };
+        },
+      
+        render: function() {
+          return (
+            //...
+            <input value={this.state.inputValue} onChange={this.updateInputValue}/>
+            //...
+          );
+        },
+      
+        updateInputValue: function(evt) {
+          this.setState({
+            inputValue: evt.target.value
+          });
+        }
+      });
+      
+    render() {
+        return (
+            <div class="container">  
+                <div class="col-lg-6 col-md-6 col-sm-8  loginbox">
+                    <div class=" row">
+                        <div class="col-md-12">
+                            <div class="singtext">Sherminal</div>   
+                        </div>
+                                 
+                    </div>
+                    <div class=" row loginbox_content ">                        
+                        <div class="col-md-12" >
+                            <input class="form-control" type="text" placeholder="User name"  />
+                        </div>
+                    </div>
+                    <div class="row ">                   
+                        <div class="col-md-12 login-btn-container">                        
+                            <a href="#" class=" btn btn-secondary">Sign in <span class="glyphicon glyphicon-log-in"></span> </a> 
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+
+
+export default LoginHandler;
+
--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import LoginHandler from './LoginHandler';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<LoginHandler />, document.getElementById('login'));
+
 registerServiceWorker();
