15eec81e19374c6f327a6f71a06efd6dd3e334fe
Loosper <boian4o1@gmail.com>
2018-03-08 21:55:21 +0200

    Get messages from the server

--- client/src/UserBar.js
+++ client/src/UserBar.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import User from './User';
 
-import axios from 'axios';
+// import axios from 'axios';
 
 
 class UserBar extends Component {
@@ -11,7 +11,16 @@ class UserBar extends Component {
         this.state = {
             users: []
         };
+        this.events = null;
         this.childid = 0;
+    }
+
+    makeUser(username) {
+        return <User
+            username={username}
+            create_terminal={this.props.terminal_factory}
+            key={this.childid++}
+        />;
     }
 
     componentDidMount() {
@@ -19,32 +28,80 @@ class UserBar extends Component {
 
         let self = this;
 
-        axios.get(url).then(function (response) {
-            // console.log(response);
+        let events = new EventSource(url); // ,{withCredentials: true});
+        this.events = events;
+        // events.withCredentials // perhaps cookies?
 
-            let activeUsers = response.data['active_users'];
+        events.onerror = function(error) {
+            // if this is not done, it will retry the connection
+            error.target.close();
+        };
 
-            if (activeUsers.length === 1) {
-                activeUsers.push('No active users.');
-            } else {
-                let new_state = self.state.users.slice();
+        events.onopen = function(event) {
+            console.log(event);
+        };
 
-                activeUsers.forEach(function(username) {
-                    new_state.push(
-                        <User
-                            username={username}
-                            create_terminal={self.props.terminal_factory}
-                            key={self.childid++}
-                        />
-                    );
-                });
+        // i hope this is fired only on unnamed events
+        events.onmessage = function(event) {
+            console.log('1' + event.data);
+            let users = JSON.parse(event.data);
+            let user_state = [];
 
-                self.setState({users: new_state});
+            for (let user in users) {
+                user_state.push(self.makeUser(user));
             }
 
-        }).catch(function (error) {
-            console.log(error);
+            self.setState({users: user_state});
+        };
+
+        events.addEventListener('added', function(event) {
+            console.log('2' + event.data);
+            let new_state = self.state.users.slice();
+
+            new_state.push(self.makeUser(event.data));
+
+            self.setState({users: new_state});
         });
+
+        events.addEventListener('removed', function(event) {
+            console.log('3' + event.data);
+            let new_state = self.state.users.slice();
+            let index = new_state.indexOf(event.data);
+
+            new_state.splice(index, 1);
+            self.setState({users: new_state});
+        });
+
+        // axios.get(url).then(function (response) {
+        //     // console.log(response);
+        //
+        //     let activeUsers = response.data['active_users'];
+        //
+        //     if (activeUsers.length === 1) {
+        //         activeUsers.push('No active users.');
+        //     } else {
+        //         let new_state = self.state.users.slice();
+        //
+        //         activeUsers.forEach(function(username) {
+        //             new_state.push(
+        //                 <User
+        //                     username={username}
+        //                     create_terminal={self.props.terminal_factory}
+        //                     key={self.childid++}
+        //                 />
+        //             );
+        //         });
+        //
+        //         self.setState({users: new_state});
+        //     }
+        //
+        // }).catch(function (error) {
+        //     console.log(error);
+        // });
+    }
+
+    componenwillUnmount() {
+        this.events.close();
     }
 
     render() {
