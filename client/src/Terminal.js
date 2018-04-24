197925f05f8c1006053b423379cc45fb59ef7818
denishristov <plaritus@gmail.com>
2018-04-24 11:39:18 +0300

    migrated to react-xterm and fixed xterm.fit

--- client/src/Terminal.js
+++ client/src/Terminal.js
@@ -1,33 +1,29 @@
 import React, { Component } from 'react';
-
+import Xterm  from 'react-xterm';
 
 import 'xterm/dist/xterm.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './styles/main.css';
 
-// sadly the other way is broken
-const Xterm = require('xterm/dist/xterm.js');
-const terminado = require('xterm/dist/addons/terminado/terminado');
-const fit = require('xterm/dist/addons/fit/fit');
-
+const Draggable = require('react-draggable');
 const CloseButton = require('./images/close-button.png');
 
-Xterm.applyAddon(terminado);
-Xterm.applyAddon(fit);
-
-const Draggable = require('react-draggable')
-
-class Terminal extends Component {
+class Terminal extends React.Component {
     constructor(props) {
         super(props);
         this.requestWrite = this.requestWrite.bind(this);
 
         //this.sizeContainer = this.props.size === 2 ? "col-md-6" : "col-md-12";
 
-        this.xterm = null;
         this.socket = null;
     }
+
+    bindXterm(xterm) {
+        if (xterm !== null) {
+            this.xtermRef = xterm.getTerminal();
+        }
+      }
 
     // TODO: dont allow this for my own termianl
     requestWrite(event) {
@@ -35,44 +31,48 @@ class Terminal extends Component {
     }
 
     componentDidMount() {
-        this.xterm = new Xterm();
+        this.xtermRef.fit()
+
         let socketURL = encodeURI('ws://' + process.env.REACT_APP_HOST +
             '/websocket/' + this.props.socketURL + '/' + this.props.authToken);
         this.socket = new WebSocket(socketURL);
         this.props.setSocket(this.socket);
         this.socket.addEventListener('close', (e) => this.props.tearDown(this));
-        this.xterm.terminadoAttach(this.socket);
-
-        // is there a way to pass a ract element?
-        this.xterm.open(document.getElementById(
-            'terminal-container' + this.props.terminalId
-        ));
+        this.xtermRef.terminadoAttach(this.socket);
     }
 
     // TODO: decide whether this should close the socket
     componentWillUnmount() {
         // this.socket.close();
         // this might be unnecessary
-        this.xterm.destroy();
+        this.xtermRef.destroy();
     }
 
     render() {
-        return (<Draggable>
-            <div className="col-md-6 terminal-col">
-                <div className="terminal-window border-top border-white terminal-color">
-                    <div className="row terminal-menu text-center">
-                        <div className="close-button"onClick={event => this.props.tearDown(this)}>
-                            <img alt="close-button" src={CloseButton} className="close-icon">
-                            </img>
+        // TODO: fix z-index
+        return (
+            <Draggable>
+                <div className="col-md-6 terminal-col">
+                    <div className="terminal-window border-top border-white terminal-color">
+                        <div className="row terminal-menu text-center">
+                            <div className="close-button" onClick={event => this.props.tearDown(this)}>
+                                <img alt="close-button" src={CloseButton} className="close-icon">
+                                </img>
+                            </div>
+                            <div onClick={this.requestWrite} className="col terminal-username">
+                                {this.props.userName}
+                            </div>
                         </div>
-                        <div onClick={this.requestWrite} className="col terminal-username">
-                            {this.props.userName}
+                        <div id={'terminal-container' + this.props.terminalId} className="border-top border-secondary">
+                            <Xterm 
+                                addons={['fit', 'fullscreen', 'terminado']}
+                                ref={xterm => this.bindXterm(xterm)}
+                            />
                         </div>
                     </div>
-                    <div id={'terminal-container' + this.props.terminalId} className="border-top border-secondary"/>
                 </div>
-            </div>
-        </Draggable>);
+            </Draggable>
+        );
     }
 }
 
