f116176eab6be599e336e6eac447926849f1ca41
Loosper <boian4o1@gmail.com>
2018-04-24 19:34:05 +0300

    helper for websocket dev

---
+++ server/testing/client_helper.py
@@ -0,0 +1,54 @@
+from tornado.websocket import websocket_connect
+
+import tornado
+import requests
+import json
+# import asyncio
+
+HOST = 'localhost:8765/'
+
+
+# this is not a testing suite. However i need to automate the testing process
+# of this whole this, so here we are
+class Tester:
+    def __init__(self):
+        response = requests.post(
+            'http://' + HOST + 'login', json={'username': 'test'}
+        )
+        login_info = response.json()
+        self.conn = websocket_connect(
+            'ws://' + HOST +
+            'websocket/' + login_info['terminal_path'] +
+            '/' + login_info['auth_token']
+        )
+
+        self.loop = tornado.ioloop.IOLoop.instance()
+
+    async def listen_websocket(self):
+        self.conn = await self.conn
+
+        # self.loop.add_callback(self.send_message, ['', ''])
+
+        while True:
+            msg = await self.conn.read_message()
+            if msg is None:
+                break
+
+            print(msg)
+
+    async def send_message(self, message):
+        self.conn.write_message(json.dumps(message))
+
+    def run(self):
+        self.loop.add_callback(self.listen_websocket)
+
+        try:
+            self.loop.start()
+        except KeyboardInterrupt:
+            print("Shutting down...")
+        finally:
+            self.loop.close()
+
+
+tester = Tester()
+tester.run()
